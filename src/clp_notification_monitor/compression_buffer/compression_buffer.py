774814b5149f1ce567a606179c89db2c726f1d72
Bingran Hu <bingranhu98@gmail.com>
2023-10-19 19:54:33 -0400

    Add support for grouping compression jobs by paths under specific prefix (#9)

--- src/clp_notification_monitor/compression_buffer/compression_buffer.py
+++ src/clp_notification_monitor/compression_buffer/compression_buffer.py
@@ -1,8 +1,20 @@
 from datetime import datetime, timedelta
 from logging import Logger
-from pathlib import PurePath
+from pathlib import Path, PurePath
 from threading import Condition, Lock
-from typing import List, Optional
+from typing import Dict, List, Optional, Tuple
+
+
+class SingleBuffer:
+    def __init__(self) -> None:
+        self.path_list: List[PurePath] = []
+        self.total_buffer_size: int = 0
+        self.first_path_timestamp: Optional[datetime] = None
+
+    def clear(self) -> None:
+        self.path_list.clear()
+        self.total_buffer_size = 0
+        self.first_path_timestamp = None
 
 
 class CompressionBuffer:
@@ -11,23 +23,20 @@ class CompressionBuffer:
         logger: Logger,
         max_buffer_size: int,
         min_refresh_period: int,
-    ):
+        group_by_paths_under_prefix: Path,
+    ) -> None:
         self._logger: Logger = logger
-
-        self.__path_list: List[PurePath] = []
-        self.__total_buffer_size: int = 0
-        self.__first_path_timestamp: Optional[datetime] = None
 
         self.__max_buffer_size: int = max_buffer_size
         self.__min_refresh_period: timedelta = timedelta(milliseconds=min_refresh_period)
 
+        self.__named_buffers: Dict[str, SingleBuffer] = {}
+        self.__ready_buffers: List[str] = []
+        self.__group_by_paths_under_prefix: Path = group_by_paths_under_prefix
+
+        # TODO: change the granularity of lock to each buffer
         self.__lock: Lock = Lock()
         self.__populate_buffer_cv: Condition = Condition(self.__lock)
-
-    def clear_buffer(self) -> None:
-        self.__path_list = []
-        self.__total_buffer_size = 0
-        self.__first_path_timestamp = None
 
     def append(
         self,
@@ -36,50 +45,106 @@ class CompressionBuffer:
         process_timestamp: datetime,
     ) -> None:
         with self.__populate_buffer_cv:
-            self.__total_buffer_size += object_size
-            if self.__first_path_timestamp is None:
-                self.__first_path_timestamp = process_timestamp
-            self.__path_list.append(s3_path)
+            relative_path: PurePath
+            index_name: str
+            try:
+                relative_path = s3_path.relative_to(self.__group_by_paths_under_prefix)
+                index_name = relative_path.parts[0]
+            except (KeyError, ValueError):
+                # This file does not belong to any named buffer
+                index_name = ""
+
+            # Get the correct buffer
+            target_buffer: SingleBuffer
+            try:
+                target_buffer = self.__named_buffers[index_name]
+            except KeyError:
+                target_buffer = SingleBuffer()
+                self.__named_buffers[index_name] = target_buffer
+
+            # Add path to buffer
+            target_buffer.total_buffer_size += object_size
+            if None is target_buffer.first_path_timestamp:
+                target_buffer.first_path_timestamp = process_timestamp
+            target_buffer.path_list.append(s3_path)
+
+            # Notify the thread waiting for a compression job
             self.__populate_buffer_cv.notify_all()
 
     def wait_for_compression_jobs(self) -> None:
         """
         Block the current process until the buffer is populated.
         """
+        # If the lock is being acquired due to unprocessed ready-to-comopress
+        # buffers, skip waiting to avoid deadlock.
+        if len(self.__ready_buffers) > 0:
+            return
+
         with self.__populate_buffer_cv:
-            while None is self.__first_path_timestamp:
+            while True:
+                # TODO: come up with a more efficient scanning method when
+                # a large number of buffers is to be expected
+                all_buffers_empty: bool = True
+                for buf in self.__named_buffers.values():
+                    if None is not buf.first_path_timestamp:
+                        all_buffers_empty = False
+                        break
+                if not all_buffers_empty:
+                    break
                 self.__populate_buffer_cv.wait()
 
-    def ready_for_compression(self) -> bool:
-        if None is self.__first_path_timestamp:
-            return False
-        if self.__total_buffer_size > self.__max_buffer_size:
-            self._logger.info(
-                "Ready for compression (max buffer size exceeded). "
-                f"Total buffer size: {self.__total_buffer_size}. "
-                f"Max buffer size: {self.__max_buffer_size}."
-            )
-            return True
-        total_elapsed_time = datetime.now() - self.__first_path_timestamp
-        if total_elapsed_time > self.__min_refresh_period:
-            self._logger.info(
-                "Ready for compression (min refresh period exceeded). "
-                f"Total elapsed time: {total_elapsed_time.total_seconds()} seconds. "
-                f"First chunk timestamp: {self.__first_path_timestamp}."
-            )
-            return True
-        return False
+    def _get_buffers_ready_for_compression(self) -> List[str]:
+        ready_buffers: List[str] = []
+        for buf_name, buf in self.__named_buffers.items():
+            if None is buf.first_path_timestamp:
+                continue
 
-    def get_paths_to_compress(self) -> List[PurePath]:
+            # Job triggered by reaching maximum archive size
+            if buf.total_buffer_size > self.__max_buffer_size:
+                self._logger.info(
+                    f"Buffer {buf_name} ready for compression (max buffer size exceeded). "
+                    f"Total buffer size: {buf.total_buffer_size}. "
+                    f"Max buffer size: {self.__max_buffer_size}."
+                )
+                ready_buffers.append(buf_name)
+                continue
+
+            # Job triggered by the minimum buffer refresh frequency
+            total_elapsed_time: timedelta = datetime.now() - buf.first_path_timestamp
+            if total_elapsed_time > self.__min_refresh_period:
+                self._logger.info(
+                    f"Buffer {buf_name} ready for compression (min refresh period exceeded). "
+                    f"Total elapsed time: {total_elapsed_time.total_seconds()} seconds. "
+                    f"First chunk timestamp: {buf.first_path_timestamp}."
+                )
+                ready_buffers.append(buf_name)
+
+        return ready_buffers
+
+    def get_paths_to_compress(self) -> Tuple[List[PurePath], str]:
         """
         :return: The path list of the current buffer if the compression job can
         be fired. Otherwise, returns an empty list.
+        Also returns the index name that applies to all the paths in this job.
         """
-        if not self.ready_for_compression():
-            return []
+        buffer_name: str = ""
+        if 0 == len(self.__ready_buffers):
+            self.__lock.acquire()
+            self.__ready_buffers = self._get_buffers_ready_for_compression()
+            if 0 == len(self.__ready_buffers):
+                self.__lock.release()
+                return [], buffer_name
 
-        with self.__lock:
-            # Get a deepcopy of the current path list
-            path_list_to_return = list(self.__path_list)
-            self.clear_buffer()
-        return path_list_to_return
+        buffer_name = self.__ready_buffers.pop(0)
+        target_buffer: SingleBuffer = self.__named_buffers[buffer_name]
+
+        # Get a deepcopy of the current path list
+        path_list_to_return = list(target_buffer.path_list)
+        target_buffer.clear()
+
+        # Do not release the lock until all target buffers have been processed.
+        # The caller is expected to repeatedly call this function for new jobs.
+        if 0 == len(self.__ready_buffers):
+            self.__lock.release()
+
+        return path_list_to_return, buffer_name
